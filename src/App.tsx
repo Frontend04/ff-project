a615be4a12486f71edf18e1a4b900ccbc4cfb746
USER <jsdev04@gmail.com>
2023-06-03 12:29:21 +0600

    made a imports images

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Food from './components/Food';
+import HamBurgerImage from './assets/hamBurger.png';
+import CheeseBurgerImage from './assets/CheeseBurger.png'
+import FriesImage from './assets/fries.png'
+import CoffeeImage from './assets/coffee.png'
+import TeaImage from './assets/tea.png'
+import ColaImage from './assets/cola.png'
 
-function App() {
+interface OrderItem {
+  name: string;
+  price: number;
+  count: number;
+}
+
+
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
-}
+};
 
 export default App;
